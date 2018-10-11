31ddc142ce7e4dc38ed69b75734f820641211c0a
Gustavo <gugabrilhante@gmail.com>
2018-10-11 16:14:40 -0400

    Initial commit

---
+++ MVVMC-Architecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMC-Architecture
+//
+//  Created by Natan on 10/11/18.
+//  Copyright © 2018 Gustavo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
